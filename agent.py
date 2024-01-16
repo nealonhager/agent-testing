569fdeeac5314a5bf8728f806eb254c3ff8fb3eb
Nealon Hager <nealonhager@gmail.com>
2024-01-15 16:22:19 -0800

    Add environment variable for GPT model, update .gitignore, add tests

--- agent.py
+++ agent.py
@@ -1,19 +1,58 @@
 from dotenv import load_dotenv
 from openai import OpenAI
+from typing import Optional
 import names
+import json
+import os
 
 
 load_dotenv()
 
 
 class Agent:
-    def __init__(self):
-        self.openai_client = None
+    def __init__(self, purpose: Optional[str] = None):
         self.identifier = names.get_full_name()
+        self.purpose = purpose
+        self._openai_client = OpenAI()
 
-    def _get_openai_client(self) -> OpenAI:
-        if self.openai_client is None:
-            self.openai_client = OpenAI()
-        
-        return self.openai_client
-        
+    def __repr__(self):
+        return f"{json.dumps({'identifier':self.identifier, 'purpose': self.purpose})}"
+
+    def x(self):
+        completion = self._openai_client.chat.completions.create(
+            model=os.getenv("GPT_MODEL"),
+            messages=[
+                {
+                    "role": "system",
+                    "content": f"You are a helpful assistant with the alias of 'f{self.identifier}'.",
+                },
+                {"role": "user", "content": "Hello, my name is Tim, what's your name?"},
+            ],
+        )
+
+        print(completion.choices[0].message)
+
+    def save(self):
+        """
+        Saves agent info to a file.
+        """
+        filter_out_keys = ["_openai_client"]
+        filtered_dict = {
+            k: v for k, v in self.__dict__.items() if k not in filter_out_keys
+        }
+        with open(f"{self.identifier}.json", "w+") as f:
+            f.write(json.dumps(filtered_dict, indent=2))
+
+    def load(self, file_name: str):
+        """
+        Loads an agent file to the agent.
+        """
+        if not file_name.endswith(".json"):
+            raise Exception("Cannot open agent save file, not a .json file.")
+
+        filter_out_keys = ["_openai_client"]
+        with open(f"{file_name}", "r+") as f:
+            data = json.loads(f.read())
+            for k, v in data.items():
+                if k not in filter_out_keys:
+                    self.__setattr__(k, v)
---
+++ tests/test_agent.py
@@ -0,0 +1,38 @@
+import pytest
+import os
+from agent import Agent
+
+
+def delete_file(file_name: str):
+    try:
+        os.remove(file_name)
+    except FileNotFoundError:
+        print(f"File not found: {file_name}")
+
+
+@pytest.fixture
+def agent1():
+    agent = Agent()
+    yield agent
+
+
+@pytest.fixture
+def agent2():
+    agent = Agent()
+    yield agent
+
+
+def test_save_agent(agent1):
+    agent1.save()
+    expected_file_name = agent1.identifier + ".json"
+    assert expected_file_name in os.listdir(".")
+    delete_file(agent1.identifier + ".json")
+
+
+def test_load_agent(agent1, agent2):
+    agent1.save()
+    assert agent1 != agent2
+    agent2.load(agent1.identifier + ".json")
+    assert str(agent1) == str(agent2)
+
+    delete_file(agent1.identifier + ".json")
